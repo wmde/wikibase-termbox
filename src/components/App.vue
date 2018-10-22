801bf0d2db05d8be7c265996df15dd60d9f9a928
bitPogo <matthias.geisler@wikimedia.de>
2018-10-22 18:54:59 +0200

    Add mw-object-mockup in dev mode

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    {{getMessage()}}
+    {{message}}
+	{{accessEntityData()}}
     <TermBox/>
   </div>
 </template>
@@ -8,6 +9,12 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+// tslint:disable-next-line
+'#if DEV === true';
+import EntityMockup from './lib/mwObject';
+const mw = new EntityMockup();
+// tslint:disable-next-line
+'#endif';
 import TermBox from './TermBox.vue';
 
 @Component( {
@@ -16,17 +23,10 @@ import TermBox from './TermBox.vue';
   },
 } )
 export default class App extends Vue {
-	public getMessage() {
-		let Return = '';
-		// tslint:disable-next-line
-		'#if DEV === true';
-		Return = 'Hallo Welt!';
-		// tslint:disable-next-line
-		'#else';
-		Return = 'Hello world!';
-		// tslint:disable-next-line
-		'#endif';
-		return Return;
+	public message = 'Hello World!';
+
+	public accessEntityData() {
+		console.log( mw.config );
 	}
 }
 </script>
---
+++ src/components/lib/mwConfig.ts
@@ -0,0 +1,15 @@
+import * as Entity from './Q64_data.json';
+
+const Data = Entity as any;
+
+export default class MWConfig {
+	public EntityData: any = '';
+
+	constructor() {
+		this.EntityData = Data;
+	}
+
+	public get( ignoreMe: string ) {
+		return this.EntityData;
+	}
+}
