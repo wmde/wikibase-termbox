f64542f0584a7072bdba98b6675da7442136c423
bitPogo <matthias.geisler@wikimedia.de>
2018-10-22 18:54:59 +0200

    Add mw-object-mockup in dev mode

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     {{message}}
+<<<<<<< HEAD
+=======
+	{{accessEntityData()}}
+>>>>>>> Add mw-object-mockup in dev mode
     <TermBox/>
   </div>
 </template>
@@ -8,6 +12,12 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+// tslint:disable-next-line
+'#if DEV === true';
+import EntityMockup from './lib/mwObject';
+const mw = new EntityMockup();
+// tslint:disable-next-line
+'#endif';
 import TermBox from './TermBox.vue';
 
 @Component( {
@@ -16,6 +26,7 @@ import TermBox from './TermBox.vue';
   },
 } )
 export default class App extends Vue {
+<<<<<<< HEAD
 	public message: string = '';
 
 	constructor() {
@@ -34,6 +45,12 @@ export default class App extends Vue {
 		// tslint:disable-next-line
 		'#endif';
 		return Return;
+=======
+	public message = 'Hello World!';
+
+	public accessEntityData() {
+		console.log( mw.config );
+>>>>>>> Add mw-object-mockup in dev mode
 	}
 }
 </script>
