d689b51916e8cc8a7d4428dd4ef58bec207b6a93
bitPogo <matthias.geisler@wikimedia.de>
2018-11-02 10:42:48 +0100

    cleanup EntityProperties

--- src/store/Entity/EntityProperties.ts
+++ src/store/Entity/EntityProperties.ts
@@ -6,20 +6,5 @@ export default interface EntityProperties {
 	Labels: DictionaryInterface<string>;
 	Descriptions: DictionaryInterface<string>;
 	Aliases: DictionaryInterface<string[]>;
-	/* Mangement Variables */
 	IsInit: boolean;
-	/*
-	getTermId(): string;
-	getTermType(): string;
-	getTermLabels(): DictionaryInterface<string>;
-	getTermLabel(): [string, string];
-	getTermDescriptions(): DictionaryInterface<string>;
-	getTermDescription(): [string, string];
-	getTermAliases(): DictionaryInterface<string[]>;
-	getTermAlias(): [string, string[]];
-/*	public getLabelByLanguageKey( LanguageKey: string ): string;
-	public getDescriptionByLanguageKey( LanguageKey: string ): string;
-	public getAliasByLanguageKey( LanguageKey: string ): string[];*/
-	/* Mutations */
-	/* Actions */
 }
