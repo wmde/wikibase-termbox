c4b478e5796cd05f77833e127f254a44cb7cd148
bitPogo <matthias.geisler@wikimedia.de>
2018-11-07 15:00:03 +0100

    Cleanup

--- src/client-entry.ts
+++ src/client-entry.ts
@@ -3,8 +3,7 @@ import App from '@/components/App.vue';
 import store from '@/store/StoreBundle';
 
 Vue.config.productionTip = false;
-console.log( store );
-store.commit( 'initilizeLanguageLabels', [ 12 ] );
+
 new Vue( {
 	store,
 	render: ( h ) => h( App ),
